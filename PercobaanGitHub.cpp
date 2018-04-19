7f4b98fa6179bbf63ac237748e581283b9008837
Patar Martua Doli Siahaan <patarismyname@gmail.com>
2018-04-19 21:21:01 +0700

    Percobaan

---
+++ PercobaanGitHub.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+
+Mami~~
+	Buatlah sebuah fungsi dengan nama tambah untuk menjumlahkan 2 buah bilangan
+	Buatlah sebuah fungsi dengan nama kali untuk mengkalikan 2 buah bilangan 
+
+Brenda
+	Buatlah sebuah fungsi dengan nama kurang untuk mengurangkan 2 buah bilangan
+	Buatlah sebuah fungsi dengan nama bagi untuk membagikan 2 buah bilangan 
+
+
+NB	: Hanya fungsi...
+*/
+	
+
+
+int main(){
+	
+	
+}
