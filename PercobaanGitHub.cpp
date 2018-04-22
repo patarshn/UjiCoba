293c9ed39e8a382f77f64b169ffe46625b2fdce8
NaomyTiaraDewi <38529620+NaomyTiaraDewi@users.noreply.github.com>
2018-04-22 21:40:30 +0700

    del

--- PercobaanGitHub.cpp
+++ PercobaanGitHub.cpp
@@ -7,13 +7,6 @@ using namespace std;
 Mami~~
 	Buatlah sebuah fungsi dengan nama tambah untuk menjumlahkan 2 buah bilangan
 	Buatlah sebuah fungsi dengan nama kali untuk mengkalikan 2 buah bilangan 
-
-Brenda
-	Buatlah sebuah fungsi dengan nama kurang untuk mengurangkan 2 buah bilangan
-	Buatlah sebuah fungsi dengan nama bagi untuk membagikan 2 buah bilangan 
-
-
-NB	: Hanya fungsi...
 */
 	
 #include<iostream>
