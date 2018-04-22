5f5d74e85bfcb126ca673fc867dbb4470067caa9
NaomyTiaraDewi <38529620+NaomyTiaraDewi@users.noreply.github.com>
2018-04-22 16:58:03 +0700

    fungsi jumlah dan bagi

--- PercobaanGitHub.cpp
+++ PercobaanGitHub.cpp
@@ -18,6 +18,14 @@ NB	: Hanya fungsi...
 	
 #include<iostream>
 using namespace std;
+
+int jumlah(int p, int s){
+	int sum=p+s;
+	return sum;}
+int bagi(int n, int d){
+	int sum=n/d;
+	return sum;}
+
 int kurang(int a, int b){
 	int jumlah=a-b;
 	return jumlah;}
